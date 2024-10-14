3960a5de92e1d07105687e6b181ec3bdfc614bc6
wonho060502 <dl060502@gamil.com>
2024-10-14 13:55:20 +0900

    로그아웃활성화

--- app/(providers)/(root)/layout.tsx
+++ app/(providers)/(root)/layout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Footer from "./_components/Footer/Footer";
 import Header from "./_components/Header/Header";
+import AuthProvider from "./_components/AuthProvider";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div>
 			<Header />
-			{children}
+			<AuthProvider>
+				{children}
+			</AuthProvider>;
 			<Footer />
 		</div>
 	);
