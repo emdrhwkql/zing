f8434755fd99091c6fba9b284fafd53895e9cd2b
jh-p632 <jh.p.6032@gmail.com>
2024-10-29 14:36:00 +0900

    feat/ui-design

--- app/(providers)/(root)/(home)/page.tsx
+++ app/(providers)/(root)/(home)/page.tsx
@@ -2,49 +2,53 @@ import api from "@/api/api";
 import Page from "@/components/Page";
 import CategoriesHomeList from "../_components/category/CategoryList/CategoriesHomeList";
 import LecturesList from "../_components/lectures/LecturesList/LecturesList";
-import LoungesList from "../_components/lounge/LoungesList/LoungesList";
-import FreeLoungePostsList from "../_components/post/FreeLoungePostsList/FreeLoungePostsList";
+import PopularLoungesList from "../_components/lounge/LoungesList/PopularLoungesList";
+import FreeLoungePostsList from "../_components/post/PostsList/FreeLoungePostsList";
+import PopularPostsList from "../_components/post/PostsList/PopularPostsList";
 
 async function HomePage() {
-  const lounges = await api.posts.getAllLounges();
+	const lounges = await api.lounges.getAllLounges();
 
-  const loungeId = 0;
-  const freePosts = await api.posts.getPostsByLoungeId(loungeId);
+	const loungeId = 0;
+	const freePosts = await api.posts.getPostsByLoungeId(loungeId);
 
-  return (
-    <Page>
-      <div className="flex flex-row justify-center">
-        <div className=" flex flex-col items-center gap-y-10 p-4 rounded-md">
-          <FreeLoungePostsList posts={freePosts} />
+	const posts = await api.posts.getPosts();
+	const noFreePosts = posts.filter((post) => post.loungeId !== 0);
 
-          <PopularLoungeList lounges={lounges} />
+	freePosts.sort((postA, postB) => postB.likes.length - postA.likes.length);
+	noFreePosts.sort((postA, postB) => postB.likes.length - postA.likes.length);
 
+	return (
+		<Page>
+			<div className="flex flex-row justify-center">
+				<div className=" flex flex-col items-center gap-y-10 p-4 rounded-md">
+					<FreeLoungePostsList posts={freePosts} />
 
-          <CategoriesHomeList />
+					<PopularPostsList posts={posts} />
 
-          <LecturesList isShowList={true} isShowSeeMore={true} />
-        </div>
+					<CategoriesHomeList />
 
-        <div className="h-full flex flex-col items-center gap-y-6 p-3 rounded-md">
-          <div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
-            내가 팔로우한 라운지
-          </div>
+					<LecturesList isShowList={true} isShowSeeMore={true} />
+				</div>
 
-          <div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
-            인기 라운지 글
-          </div>
+				<div className="h-full flex flex-col items-center gap-y-6 p-3 rounded-md">
+					<div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
+						내가 팔로우한 라운지
+					</div>
 
-          <div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
-            내가 팔로우한 라운지
-          </div>
+					<PopularLoungesList lounges={lounges} />
 
-          <div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
-            내가 팔로우한 라운지
-          </div>
-        </div>
-      </div>
-    </Page>
-  );
+					<div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
+						내가 팔로우한 라운지
+					</div>
+
+					<div className="rounded-xl w-96 h-56  bg-white grid place-items-center">
+						내가 팔로우한 라운지
+					</div>
+				</div>
+			</div>
+		</Page>
+	);
 }
 
 export default HomePage;
--- app/(providers)/(root)/_components/category/CategoryList/CategoriesHomeList.tsx
+++ app/(providers)/(root)/_components/category/CategoryList/CategoriesHomeList.tsx
@@ -1,57 +1,65 @@
 "use client";
 
 import MainBox from "@/components/MainBox";
+import arrayShuffle from "array-shuffle";
+import { random } from "nanoid";
 import Link from "next/link";
 import useCategoriesList from "./CategoriesList.hooks";
 
 function CategoriesHomeList() {
-  const { categories } = useCategoriesList();
+	const { categories } = useCategoriesList();
 
-  // 카테고리 목록에서 자유 게시판 안보이게 처리
-  const noFreeCategory = categories?.filter((category) => category.id !== 0);
+	// 카테고리 목록에서 자유 게시판 안보이게 처리
+	const noFreeCategory = categories?.filter((category) => category.id !== 0);
 
-  return (
-    <MainBox>
-      <div className="mb-4 pb-4  border-b">
-        <Link href={"/categories"}>
-          <h1 className="font-bold text-2xl">카테고리</h1>
-        </Link>
-      </div>
+	noFreeCategory!.sort((a) => (random(a.id) ? 1 : -1));
 
-      <ul
-        id="slider"
-        className="flex flex-row gap-x-8 justify-between items-center"
-      >
-        {noFreeCategory
-          ?.map((category) => (
-            <li
-              key={category.id}
-              className="relative w-40 p-4 pb-8 rounded-2xl bg-black overflow-hidden"
-            >
-              <img
-                src={`https://vcvunmefpfrcskztejms.supabase.co/storage/v1/object/public/category_images/${category.categoryImg}`}
-                className="absolute top-0 left-0 w-full h-full opacity-45 scale-150"
-              />
+	const shuffledCategories = arrayShuffle(noFreeCategory!);
 
-              <Link
-                href={`/categories/${category.id}`}
-                className="grid grid-cols-1 place-items-center z-10"
-              >
-                <img
-                  src={`https://vcvunmefpfrcskztejms.supabase.co/storage/v1/object/public/category_images/${category.categoryImg}`}
-                  className="w-32 h-32 bg-[#fdfbfc] rounded-full opacity-90 z-10"
-                />
+	return (
+		<MainBox>
+			<div className="mb-4 pb-4 border-b">
+				<Link href={"/categories"}>
+					<h1 className="w-40 font-bold text-2xl hover:text-[27px] hover:duration-150">
+						카테고리
+					</h1>
+				</Link>
+			</div>
 
-                <p className="font-bold text-2xl text-white text-center pt-2 z-10">
-                  {category.categoryName}
-                </p>
-              </Link>
-            </li>
-          ))
-          .slice(0, 5)}
-      </ul>
-    </MainBox>
-  );
+			<ul
+				id="slider"
+				className="flex flex-row gap-x-8 justify-between items-center"
+			>
+				{shuffledCategories
+					?.map((category) => (
+						<li
+							key={category.id}
+							className="relative w-40 p-4 pb-8 rounded-2xl bg-black overflow-hidden hover:-translate-y-3 hover:duration-200"
+						>
+							<img
+								src={`https://vcvunmefpfrcskztejms.supabase.co/storage/v1/object/public/category_images/${category.categoryImg}`}
+								className="absolute top-0 left-0 w-full h-full opacity-45 scale-150"
+							/>
+
+							<Link
+								href={`/categories/${category.id}`}
+								className="grid grid-cols-1 place-items-center z-10"
+							>
+								<img
+									src={`https://vcvunmefpfrcskztejms.supabase.co/storage/v1/object/public/category_images/${category.categoryImg}`}
+									className="w-32 h-32 bg-[#fdfbfc] rounded-full opacity-90 z-10"
+								/>
+
+								<p className="font-bold text-2xl text-white text-center pt-2 z-10">
+									{category.categoryName}
+								</p>
+							</Link>
+						</li>
+					))
+					.slice(0, 5)}
+			</ul>
+		</MainBox>
+	);
 }
 
 export default CategoriesHomeList;
