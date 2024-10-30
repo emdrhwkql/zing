1aa4456fe9c22aeec2c9023a3e9ff3f2101a1f14
yoojinyoung <instructor@yoojinyoung.com>
2024-10-30 10:40:44 +0900

    chore: remove unnecessary codes

--- app/(providers)/(root)/layout.tsx
+++ app/(providers)/(root)/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AuthProvider from "./_components/AuthProvider/AuthProvider";
 import Footer from "./_components/Footer/Footer";
 import Header from "./_components/Header/Header";
 
@@ -7,11 +6,10 @@ function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div>
 			<Header />
-			<AuthProvider>{children}</AuthProvider>
+			{children}
 			<Footer />
 		</div>
 	);
 }
 
 export default RootLayout;
-	
--- app/(providers)/layout.tsx
+++ app/(providers)/layout.tsx
@@ -1,7 +1,12 @@
 import TanstackQueryProvider from "@/tanstack/query/client";
+import AuthProvider from "./_providers/AuthProvider/AuthProvider";
 
 function ProvidersLayout({ children }: { children: React.ReactNode }) {
-	return <TanstackQueryProvider>{children}</TanstackQueryProvider>;
+  return (
+    <TanstackQueryProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </TanstackQueryProvider>
+  );
 }
 
 export default ProvidersLayout;
