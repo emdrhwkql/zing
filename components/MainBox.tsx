77e1a53a0f709771b60f79526c7871da12d2bfdf
jh-p632 <jh.p.6032@gmail.com>
2024-10-30 15:17:15 +0900

    feat: post 디자인

--- components/MainBox.tsx
+++ components/MainBox.tsx
@@ -1,18 +1,9 @@
 import { PropsWithChildren } from "react";
 
-function MainBox({
-	children,
-	bgColor,
-}: PropsWithChildren<{ bgColor?: string }>) {
+function MainBox({ children }: PropsWithChildren) {
 	return (
 		<div>
-			{bgColor === "none" ? (
-				<div className="p-6 rounded-xl w-[950px]">{children}</div>
-			) : (
-				<div className="p-6 rounded-xl w-[950px] bg-white">
-					{children}
-				</div>
-			)}
+			<div className="p-6 rounded-xl w-[1000px] bg-white">{children}</div>
 		</div>
 	);
 }
--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,56 +1,65 @@
 import { Tables } from "@/supabase/database.types";
 import Image from "next/image";
 import Link from "next/link";
+import { FaShareAlt } from "react-icons/fa";
+import LikeButton from "./LikeButton";
 
 interface PostFeedProps {
-  post: Tables<"posts"> & { author: Tables<"users"> };
+	post: Tables<"posts"> & { author: Tables<"users"> };
 }
 
 function PostFeed({ post }: PostFeedProps) {
-  return (
-    <article>
-      <Link
-        href={`/posts/${post.id}`}
-        className="flex flex-col max-w-sm w-full"
-      >
-        {/* Header */}
-        <div className="flex flex-row gap-x-2 items-center pb-2">
-          <div className="w-10 h-10 relative">
-            <Image
-              src={post.author.profileImg}
-              alt={post.author.userId}
-              fill
-              className="rounded-full"
-            />
-          </div>
+	return (
+		<article>
+			<Link
+				href={`/posts/${post.id}`}
+				className="flex flex-col max-w-sm w-full"
+			>
+				{/* Header */}
+				<div className="flex flex-row gap-x-2 items-center pb-2">
+					<div className="w-8 h-8 relative">
+						<Image
+							src={post.author?.profileImg}
+							alt={post.author?.userId}
+							fill
+							className="rounded-md"
+						/>
+					</div>
 
-          <span>{post.author.userName}</span>
-        </div>
+					<span>{post.author?.userName}</span>
+				</div>
 
-        {/* Image */}
-        <div>
-          <div className="aspect-square w-full relative">
-            <Image src={post.imageUrl} fill alt={post.title} />
-          </div>
-          <h6 className="font-semibold text-lg">{post.title}</h6>
+				{/* Image */}
+				<div>
+					<div className="aspect-square w-full h-56 relative">
+						<Image
+							src={post.imageUrl}
+							fill
+							alt={post.title}
+							className="object-cover"
+						/>
+					</div>
+					<h6 className="font-semibold text-lg">{post.title}</h6>
 
-          <p>{post.content}</p>
-        </div>
-        {/* Title and Content */}
+					<p>{post.content}</p>
+				</div>
+				{/* Title and Content */}
 
-        {/* Footer */}
-        <div className="flex flex-row items-center mt-2 pt-2 border-t">
-          <span className="leading-3">{post.createdAt.slice(0, 10)}</span>
+				{/* Footer */}
+				<div className="flex flex-row items-center mt-2 pt-2 border-t">
+					<span className="leading-3">
+						{post.createdAt.slice(0, 10)}
+					</span>
 
-          <div className="ml-auto flex flex-row gap-x-2 items-center">
-            {/* <LikeButton postId={post.id} />
+					<div className="ml-auto flex flex-row gap-x-2 items-center">
+						<LikeButton postId={post.id} />
 
-            <FaShareAlt /> */}
-          </div>
-        </div>
-      </Link>
-    </article>
-  );
+						<FaShareAlt />
+					</div>
+				</div>
+			</Link>
+		</article>
+	);
 }
 
 export default PostFeed;
