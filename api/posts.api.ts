b2b3ad2ba592cafb91150d0fbc6b5577b9d9dc17
yoojinyoung <instructor@yoojinyoung.com>
2024-10-30 11:55:13 +0900

    feat: CreatePostFeed and remove console.loggins

--- api/posts.api.ts
+++ api/posts.api.ts
@@ -3,136 +3,136 @@ import supabase from "@/supabase/client";
 import { User } from "@supabase/supabase-js";
 
 async function createPost(
-	title: string,
-	content: string,
-	loungeId: number,
-	currentUser: User
+  title: string,
+  content: string,
+  loungeId: number,
+  currentUser: User
 ) {
-	const response = await supabase
-		.from("posts")
-		.insert({ title, content, loungeId, userId: currentUser!.id })
-		.select("*")
-		.single();
+  const response = await supabase
+    .from("posts")
+    .insert({ title, content, loungeId, userId: currentUser!.id })
+    .select("*")
+    .single();
 
-	const post = response.data;
+  const post = response.data;
 
-	return post;
+  return post;
 }
 
 async function getPost(postId: number) {
-	const response = await supabase
-		.from("posts")
-		.select("*")
-		.eq("id", postId)
-		.single();
+  const response = await supabase
+    .from("posts")
+    .select("*")
+    .eq("id", postId)
+    .single();
 
-	const posts = response.data;
+  const posts = response.data;
 
-	if (!posts) return null;
+  if (!posts) return null;
 
-	return posts;
+  return posts;
 }
 
 async function getPosts() {
-	const response = await supabase.from("posts").select("*, likes (id)");
-	const posts = response.data;
+  const response = await supabase.from("posts").select("*, likes (id)");
+  const posts = response.data;
 
-	if (!posts) return [];
+  if (!posts) return [];
 
-	return posts;
+  return posts;
 }
 
 async function setPostImage(filepath: string, imageFile: File) {
-	const { data: postImg } = await supabase.storage
-		.from("post_images")
-		.upload(filepath, imageFile, { upsert: true });
+  const { data: postImg } = await supabase.storage
+    .from("post_images")
+    .upload(filepath, imageFile, { upsert: true });
 
-	return postImg;
+  return postImg;
 }
 
 async function updatePostImg(
-	currentUser: User,
-	imageUrl: string,
-	loungeId: number
+  currentUser: User,
+  imageUrl: string,
+  loungeId: number
 ) {
-	await supabase
-		.from("posts")
-		.update({
-			imageUrl,
-		})
-		.eq("userId", currentUser!.id)
-		.eq("id", loungeId);
+  await supabase
+    .from("posts")
+    .update({
+      imageUrl,
+    })
+    .eq("userId", currentUser!.id)
+    .eq("id", loungeId);
 }
 
 async function updatePostTitle(
-	currentUser: User,
-	title: string,
-	loungeId: number
+  currentUser: User,
+  title: string,
+  loungeId: number
 ) {
-	await supabase
-		.from("posts")
-		.update({
-			title,
-		})
-		.eq("userId", currentUser!.id)
-		.eq("id", loungeId);
+  await supabase
+    .from("posts")
+    .update({
+      title,
+    })
+    .eq("userId", currentUser!.id)
+    .eq("id", loungeId);
 }
 
 async function updatePostContent(
-	currentUser: User,
-	content: string,
-	loungeId: number
+  currentUser: User,
+  content: string,
+  loungeId: number
 ) {
-	await supabase
-		.from("posts")
-		.update({
-			content,
-		})
-		.eq("userId", currentUser!.id)
-		.eq("id", loungeId);
+  await supabase
+    .from("posts")
+    .update({
+      content,
+    })
+    .eq("userId", currentUser!.id)
+    .eq("id", loungeId);
 }
 
 async function getPostsICreated(currentUser: User) {
-	const response = await supabase
-		.from("posts")
-		.select("*")
-		.eq("userId", currentUser!.id);
+  const response = await supabase
+    .from("posts")
+    .select("*")
+    .eq("userId", currentUser!.id);
 
-	const posts = response.data;
+  const posts = response.data;
 
-	if (!posts) return [];
+  if (!posts) return [];
 
-	return posts;
+  return posts;
 }
 
 async function getPostsByLoungeId(loungeId: number) {
-	const response = await supabase
-		.from("posts")
-		.select("*, likes (id)")
-		.eq("loungeId", loungeId);
+  const response = await supabase
+    .from("posts")
+    .select("*, likes (count), author:users(*)")
+    .eq("loungeId", loungeId);
 
-	const posts = response.data;
+  const posts = response.data;
 
-	if (!posts) return [];
+  if (!posts) return [];
 
-	return posts;
+  return posts;
 }
 
 async function deletePost(post: Posts) {
-	await supabase.from("posts").delete().eq("id", post.id);
+  await supabase.from("posts").delete().eq("id", post.id);
 }
 
 const postsAPI = {
-	createPost,
-	getPost,
-	getPosts,
-	getPostsByLoungeId,
-	getPostsICreated,
-	deletePost,
-	updatePostImg,
-	updatePostContent,
-	updatePostTitle,
-	setPostImage,
+  createPost,
+  getPost,
+  getPosts,
+  getPostsByLoungeId,
+  getPostsICreated,
+  deletePost,
+  updatePostImg,
+  updatePostContent,
+  updatePostTitle,
+  setPostImage,
 };
 
 export default postsAPI;
--- app/(providers)/(root)/_components/post/PostsList/FreeLoungePostsList.tsx
+++ app/(providers)/(root)/_components/post/PostsList/FreeLoungePostsList.tsx
@@ -2,121 +2,90 @@
 
 import LikeButton from "@/components/LikeButton";
 import MainBox from "@/components/MainBox";
+import PostFeed from "@/components/PostFeed";
 import { Posts } from "@/types/posts.types";
 import Link from "next/link";
 import { useState } from "react";
 import { FaMinus, FaPlus, FaShareAlt } from "react-icons/fa";
 
 function FreeLoungePostsList({ posts }: { posts: Posts }) {
-	const [isShowMore, setIsShowMore] = useState(false);
+  const [isShowMore, setIsShowMore] = useState(false);
 
-	return (
-		<MainBox>
-			<div className="mb-4 pb-4 border-b flex flex-row">
-				<Link href={"/lounges/0"}>
-					<h1 className="font-bold text-2xl hover:text-[27px] hover:duration-150">
-						자유 게시판
-					</h1>
-				</Link>
+  return (
+    <MainBox>
+      <div className="mb-4 pb-4 border-b flex flex-row">
+        <Link href={"/lounges/0"}>
+          <h1 className="font-bold text-2xl hover:text-[27px] hover:duration-150">
+            자유 게시판
+          </h1>
+        </Link>
 
-				<p className="h-full mt-auto ml-3 font-semibold text-xl">
-					TOP 8
-				</p>
-			</div>
+        <p className="h-full mt-auto ml-3 font-semibold text-xl">TOP 8</p>
+      </div>
 
-			{isShowMore ? (
-				// 메인 페이지에 8개짜리
-				<ul className="grid grid-cols-4 gap-5 place-items-center">
-					{posts
-						.map((post) => (
-							<li key={post.id} className="w-48">
-								<div className="flex flex-row gap-x-2 items-center pb-2">
-									<div className="w-4 h-4 bg-gray-500 rounded-md" />
+      {isShowMore ? (
+        // 메인 페이지에 8개짜리
+        <ul className="grid grid-cols-4 gap-5 place-items-center">
+          {posts
+            .map((post) => (
+              <li key={post.id} className="w-48">
+                <div className="flex flex-row gap-x-2 items-center pb-2">
+                  <div className="w-4 h-4 bg-gray-500 rounded-md" />
 
-									<p>{post.userId.slice(0, 10)} • • •</p>
-								</div>
+                  <p>{post.userId.slice(0, 10)} • • •</p>
+                </div>
 
-								<Link href={`/posts/${post.id}`}>
-									<div className="w-48 h-48 bg-gray-300" />
-									<h1 className="font-semibold text-lg">
-										{post.title}
-									</h1>
+                <Link href={`/posts/${post.id}`}>
+                  <div className="w-48 h-48 bg-gray-300" />
+                  <h1 className="font-semibold text-lg">{post.title}</h1>
 
-									<p>{post.content}</p>
-								</Link>
+                  <p>{post.content}</p>
+                </Link>
 
-								<div className="flex flex-row items-center mt-2 pt-2 border-t">
-									<span className="leading-3">
-										{post.createdAt.slice(0, 10)}
-									</span>
+                <div className="flex flex-row items-center mt-2 pt-2 border-t">
+                  <span className="leading-3">
+                    {post.createdAt.slice(0, 10)}
+                  </span>
 
-									<div className="ml-auto flex flex-row gap-x-2 items-center">
-										<LikeButton postId={post.id} />
+                  <div className="ml-auto flex flex-row gap-x-2 items-center">
+                    <LikeButton postId={post.id} />
 
-										<FaShareAlt />
-									</div>
-								</div>
-							</li>
-						))
-						.slice(0, 8)}
-				</ul>
-			) : (
-				// 메인 페이지에 4개짜리
-				<ul className="grid grid-cols-4 gap-5 place-items-center">
-					{posts
-						.map((post) => (
-							<li
-								key={post.id}
-								className="w-48 hover:scale-105 hover:duration-200"
-							>
-								<div className="flex flex-row gap-x-2 items-center pb-2">
-									<div className="w-4 h-4 bg-gray-500 rounded-md" />
+                    <FaShareAlt />
+                  </div>
+                </div>
+              </li>
+            ))
+            .slice(0, 8)}
+        </ul>
+      ) : (
+        // 메인 페이지에 4개짜리
+        <ul className="grid grid-cols-4 gap-5 justify-items-stretch">
+          {posts
+            .map((post) => (
+              <li key={post.id}>
+                <PostFeed post={post} />
+              </li>
+            ))
+            .slice(0, 4)}
+        </ul>
+      )}
 
-									<p>{post.userId.slice(0, 10)} • • •</p>
-								</div>
-
-								<Link href={`/posts/${post.id}`}>
-									<div className="w-48 h-48 bg-gray-300" />
-									<h1 className="font-semibold text-lg">
-										{post.title}
-									</h1>
-
-									<p>{post.content}</p>
-								</Link>
-
-								<div className="flex flex-row items-center mt-2 pt-2 border-t">
-									<span className="leading-3">
-										{post.createdAt.slice(0, 10)}
-									</span>
-
-									<div className="ml-auto flex flex-row gap-x-2 items-center">
-										<LikeButton postId={post.id} />
-
-										<FaShareAlt />
-									</div>
-								</div>
-							</li>
-						))
-						.slice(0, 4)}
-				</ul>
-			)}
-
-			<div className="mt-3 flex justify-center relative ">
-				<button
-					onClick={() => {
-						setIsShowMore((e) => !e);
-					}}
-					className="absolute top-0 left-1/2 -translate-x-1/2 bg-[#fdfbfc] rounded-full p-2 shadow-[0_4px_4px_rgb(75,85,99)] active:scale-125 active:duration-75"
-				>
-					{isShowMore ? (
-						<FaMinus className="text-lg " />
-					) : (
-						<FaPlus className="text-lg" />
-					)}
-				</button>
-			</div>
-		</MainBox>
-	);
+      <div className="mt-3 flex justify-center relative ">
+        <button
+          onClick={() => {
+            setIsShowMore((e) => !e);
+          }}
+          className="absolute top-0 left-1/2 -translate-x-1/2 bg-[#fdfbfc] rounded-full p-2 shadow-[0_4px_4px_rgb(75,85,99)] active:scale-125 active:duration-75"
+        >
+          {isShowMore ? (
+            <FaMinus className="text-lg " />
+          ) : (
+            <FaPlus className="text-lg" />
+          )}
+        </button>
+      </div>
+    </MainBox>
+  );
 }
 
 export default FreeLoungePostsList;
