6aa971d7e147d8467e6a4f3efc2be7dcb0dbfa57
jh-p632 <jh.p.6032@gmail.com>
2024-10-31 14:52:17 +0900

    feat: modal design

--- app/(providers)/(root)/_components/lounge/LoungeDelete/LoungeDelete.tsx
+++ app/(providers)/(root)/_components/lounge/LoungeDelete/LoungeDelete.tsx
@@ -2,32 +2,37 @@
 
 import api from "@/api/api";
 import { useAuthStore } from "@/zustand/auth.store";
+import { useModalStore } from "@/zustand/modal.store";
 import { useMutation } from "@tanstack/react-query";
 import { useParams, useRouter } from "next/navigation";
 
 function LoungeDelete() {
-  const currentUser = useAuthStore((state) => state.currentUser);
-  const params = useParams();
-  const router = useRouter();
+	const currentUser = useAuthStore((state) => state.currentUser);
+	const closeModal = useModalStore((state) => state.closeModal);
+	const params = useParams();
+	const router = useRouter();
 
-  const loungeId = Number(params.loungeId);
+	const loungeId = Number(params.loungeId);
 
-  const { mutate: deleteLounge } = useMutation({
-    mutationFn: async (loungeId: number) =>
-      await api.lounges.deleteLounge(loungeId, currentUser!),
-  });
+	const { mutate: deleteLounge } = useMutation({
+		mutationFn: async (loungeId: number) =>
+			await api.lounges.deleteLounge(loungeId, currentUser!),
+	});
 
-  const handleClickDeleteLounge = () => {
-    deleteLounge(loungeId);
+	const handleClickDeleteLounge = () => {
+		deleteLounge(loungeId);
+		closeModal();
+		router.push("/");
+	};
 
-    router.push("/");
-  };
-
-  return (
-    <button onClick={handleClickDeleteLounge} className="bg-black border-black">
-      라운지 삭제
-    </button>
-  );
+	return (
+		<button
+			onClick={handleClickDeleteLounge}
+			className="bg-white rounded-md w-full hover:duration-300 hover:bg-gray-400 active:scale-95"
+		>
+			라운지 삭제
+		</button>
+	);
 }
 
 export default LoungeDelete;
--- app/(providers)/(root)/_components/lounge/LoungeMod/LoungeModName/LoungeModName.tsx
+++ app/(providers)/(root)/_components/lounge/LoungeMod/LoungeModName/LoungeModName.tsx
@@ -8,66 +8,67 @@ import { useAuthStore } from "@/zustand/auth.store";
 import { useMutation } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FaCheckSquare } from "react-icons/fa";
 
 interface UpdateName {
-  name: string;
-  loungeId: number;
+	name: string;
+	loungeId: number;
 }
 
 function LoungeModName() {
-  const currentUser = useAuthStore((state) => state.currentUser);
+	const currentUser = useAuthStore((state) => state.currentUser);
 
-  const [name, setName] = useState("");
-  const params = useParams();
+	const [name, setName] = useState("");
+	const params = useParams();
 
-  const loungeId = Number(params.loungeId);
-  const { mutate: updateName } = useMutation({
-    mutationFn: async ({ name, loungeId }: UpdateName) =>
-      api.lounges.updateLoungeName(currentUser!, name, loungeId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["users"] });
-    },
-  });
+	const loungeId = Number(params.loungeId);
+	const { mutate: updateName } = useMutation({
+		mutationFn: async ({ name, loungeId }: UpdateName) =>
+			api.lounges.updateLoungeName(currentUser!, name, loungeId),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["users"] });
+		},
+	});
 
-  useEffect(() => {
-    if (!currentUser) return;
+	useEffect(() => {
+		if (!currentUser) return;
 
-    (async () => {
-      const { data: lounges } = await supabase
-        .from("lounges")
-        .select("*")
-        .eq("userId", currentUser!.id);
+		(async () => {
+			const { data: lounges } = await supabase
+				.from("lounges")
+				.select("*")
+				.eq("userId", currentUser!.id);
 
-      if (!lounges) return;
+			if (!lounges) return;
 
-      const lounge = lounges[loungeId];
-    })();
-  }, [currentUser]);
+			const lounge = lounges[loungeId];
+		})();
+	}, [currentUser]);
 
-  const handleClickModName = () => {
-    updateName({
-      name: name,
-      loungeId: loungeId!,
-    });
-  };
+	const handleClickModName = () => {
+		updateName({
+			name: name,
+			loungeId: loungeId!,
+		});
+	};
 
-  return (
-    <>
-      <Input
-        type="text"
-        name="name"
-        onChange={(e) => setName(e.target.value)}
-        inputClassName="text-black"
-      />
+	return (
+		<div className="flex flex-row gap-x-4 w-full items-center">
+			<div className="w-full">
+				<Input
+					type="text"
+					name="name"
+					onChange={(e) => setName(e.target.value)}
+					wrapperClassName="p-2"
+					placeholder="변경할 제목을 입력해주세요."
+				/>
+			</div>
 
-      <button
-        className="rounded-full w-36 h-10 py-2 flex flex-row gap-x-2 justify-center items-center border"
-        onClick={handleClickModName}
-      >
-        <p>이름 수정하기</p>
-      </button>
-    </>
-  );
+			<button onClick={handleClickModName} className="h-full">
+				<FaCheckSquare className="w-full h-full text-white active:scale-90 hover:text-gray-400 hover:duration-300" />
+			</button>
+		</div>
+	);
 }
 
 export default LoungeModName;
